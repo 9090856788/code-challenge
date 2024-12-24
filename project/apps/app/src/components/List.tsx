239ef510011d51849ed86b0e5ac3c8cb35f6492b
9090856788 <kanhucharansahoo595@gmail.com>
2024-12-24 18:40:35 +0530

    task-5: Add remove button to each Pokémon and dispatch action to remove Pokémon

--- project/apps/app/src/components/List.tsx
+++ project/apps/app/src/components/List.tsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removePokemon } from "./redux/pokemonSlice";
 import "./List.css";
 
 interface ListProps {
   items: string[];
 }
 
-const List: React.FC<ListProps> = ({ items }) => (
-  <div className="grid-container">
-    {items.map((item) => (
-      <div key={item} className="grid-item">
-        {item}
-      </div>
-    ))}
-  </div>
-);
+const List: React.FC<ListProps> = ({ items }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = (name: string) => {
+    dispatch(removePokemon(name));
+  };
+
+  return (
+    <div className="grid-container">
+      {items.map((item) => (
+        <div key={item} className="grid-item">
+          {item}
+          <button onClick={() => handleRemove(item)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default List;
